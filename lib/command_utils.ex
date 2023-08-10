a364a050052ba7262e797703b9032931157c76a6
Jon Lauridsen <mail@jonlauridsen.com>
2023-08-11 01:41:37 +0200

    Support to_command_value calling into Jason.encode!

--- lib/command_utils.ex
+++ lib/command_utils.ex
@@ -2,4 +2,6 @@ defmodule ElixirScript.CommandUtils do
   def to_command_value(nil), do: ""
 
   def to_command_value(input) when is_binary(input), do: input
+
+  def to_command_value(input), do: Jason.encode!(input)
 end
