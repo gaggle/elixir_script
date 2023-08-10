e727618dfd2b6ad5e48be027f3b3b915f8ecc3f2
Jon Lauridsen <mail@jonlauridsen.com>
2023-08-11 01:41:58 +0200

    Pass context into script's bindings

--- lib/CLI.ex
+++ lib/CLI.ex
@@ -1,9 +1,10 @@
 defmodule ElixirScript.CLI do
+  alias ElixirScript.Context
   alias ElixirScript.Core
 
   def main(_args \\ []) do
     script = Core.get_input("script", required: true)
-    {value, _binding} = Code.eval_string(script)
+    {value, _binding} = Code.eval_string(script, context: Context.from_github_environment())
     Core.set_output("result", value)
   end
 end
