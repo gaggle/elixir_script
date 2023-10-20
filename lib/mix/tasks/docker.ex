06a068357667c6db61446697bb1f750f6497d0a7
Jon Lauridsen <mail@jonlauridsen.com>
2023-10-20 17:47:23 +0200

    Add mix tasks

---
+++ lib/mix/tasks/docker.ex
@@ -0,0 +1,16 @@
+defmodule Mix.Tasks.Docker do
+  use Mix.Task
+
+  @shortdoc "Manages Docker operations for the project."
+
+  def run(args) do
+    case args do
+      ["build"] -> build()
+      _ -> Mix.raise("Usage: mix docker build")
+    end
+  end
+
+  defp build do
+    System.cmd("docker", ["build", "--progress", "plain", "--tag", "elixir_script:latest", "-f", ".github/Dockerfile", "."])
+  end
+end
