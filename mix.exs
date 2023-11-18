ea32fdb1dd2e4ac76929d8643acfc8e779154e83
Jon Lauridsen <mail@jonlauridsen.com>
2023-11-18 17:58:55 +0100

    Add credo + dialyxir dependencies

--- mix.exs
+++ mix.exs
@@ -20,6 +20,8 @@ defmodule ElixirScript.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.4"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
