594a1dcb04b7136c09a6f58e7a2100acd83a97b9
Jon Lauridsen <mail@jonlauridsen.com>
2023-11-23 16:43:53 +0100

    Add tests for ScriptRunner

---
+++ test/script_runner_test.exs
@@ -0,0 +1,34 @@
+defmodule ElixirScript.ScriptRunnerTest do
+  use ExUnit.Case, async: true
+
+  import Mox
+
+  alias ElixirScript.ScriptRunner
+  alias Test.Fixtures.GitHubWorkflowRun
+
+  setup do
+    stub(SystemEnvBehaviourMock, :get_env, fn varname, default ->
+      GitHubWorkflowRun.env()[varname] || default
+    end)
+
+    :ok
+  end
+
+  test "run executes script in a given context and returns its output" do
+    script = """
+    "Hello, " <> context.actor
+    """
+
+    assert ScriptRunner.run(script) == "Hello, gaggle"
+  end
+
+  test "run raises if the script raises an error" do
+    script = """
+    raise "error!"
+    """
+
+    assert_raise RuntimeError, "error!", fn ->
+      ScriptRunner.run(script)
+    end
+  end
+end
